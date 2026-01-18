662fc4c54fd67153b65fcc2896505084e3ec4acb
Sowmya <sowmya.janmahanthi.001@student.uni.lu>
2026-01-18 15:27:14 +0100

    revised nix

--- rnaRAP/gen-env.R
+++ rnaRAP/gen-env.R
@@ -1,18 +1,14 @@
-
 library(rix)
+dates <- available_dates()
+latest_date <- tail(dates, 1)
 
 rix(
-  date = "2025-10-14",
-  r_pkgs = c(
-    "DESeq2",
-    "pasilla",
-    "devtools",
-    "quarto"
-  ),
-  system_pkgs = NULL,
-  git_pkgs = NULL,
+  date = latest_date,
+  r_pkgs = c("BiocManager", "dplyr", "ggplot2", "DESeq2", "pasilla", "knitr", 
+             "testthat", "apeglm", "KernSmooth", "tibble", "quarto",
+            "pheatmap", "RColorBrewer", "gridExtra", "tidyr"),
+  system_pkgs = c("pandoc"),
   ide = "none",
   project_path = ".",
-  overwrite = TRUE,
-  print = TRUE
+  overwrite = TRUE
 )
